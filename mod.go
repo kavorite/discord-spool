e6b44d8e7c67abe1bb809df29c84852c079bdaa9
kavorite <iseurie@gmail.com>
2020-04-16 17:31:12 -0400

    initial commit

---
+++ mod.go
@@ -0,0 +1,46 @@
+package spool
+
+import (
+	"io"
+
+	dgo "github.com/bwmarrin/discordgo"
+)
+
+type Head string
+
+const Zero = Head("00000000000000000000")
+
+// spool.T is the structure which encapsulates all state necessary to iterate
+// over a paginated channel message history.
+type T struct {
+	ChID string
+	Head
+}
+
+// Unroll attempts to iter() on the message history of the given Discord
+// channel ID, in descending chronological order of appearance, and blocks
+// until some error is encountered, all messages have been read (io.EOF), or
+// iter returns false.
+func (spl *T) Unroll(s *dgo.Session, iter func(*dgo.Message) bool) (err error) {
+	if spl.Head == "" {
+		spl.Head = Zero
+	}
+	msgs := make([]*dgo.Message, 0, 100)
+	for {
+		msgs, err = s.ChannelMessages(spl.ChID, 100, string(spl.Head), "", "")
+		if err != nil {
+			return
+		}
+		for _, msg := range msgs {
+			if !iter(msg) {
+				return
+			}
+		}
+		spl.Head = Head(msgs[len(msgs)-1].ID)
+		if len(msgs) < 100 {
+			err = io.EOF
+			return
+		}
+	}
+	return
+}
